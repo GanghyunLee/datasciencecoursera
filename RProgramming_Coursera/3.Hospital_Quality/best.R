f482e3d78beaf4615356638396b3e0d075a0b7c9
landscape <lee308812@gmail.com>
2017-12-01 20:58:21 +0900

    best.R Complete

---
+++ RProgramming_Coursera/3.Hospital_Quality/best.R
@@ -0,0 +1,44 @@
+best <- function(state, outcome)
+{
+        result <- c()
+        outcomeIdx <- 0
+        
+        ## Read outcome data
+        mainData <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+        validOutcome <- c("heart attack", "heart failure", "pneumonia")
+        
+        ## Check that state and outcome are valid
+        if(!state %in% mainData$State)
+        {
+                stop('invalid state')
+        }
+        
+        if(!outcome %in% validOutcome)
+        {
+                stop('invalid outcome')
+        }
+        
+        ## Return hospital name in that state with lowest 30-day death
+        if(outcome == "heart attack")
+        {
+                outcomeIdx <- 13
+        }
+        else if(outcome == "heart failure")
+        {
+                outcomeIdx <- 19
+        }
+        else
+        {
+                outcomeIdx <- 25
+        }
+        ## rate
+        mainData[,outcomeIdx] <- suppressWarnings(as.numeric(mainData[,outcomeIdx]))
+        
+        subData <- mainData[mainData[,"State"] == state, ]
+        
+        idx <- which(subData[,outcomeIdx] == min(subData[,outcomeIdx], na.rm = TRUE))
+        
+        result <- subData[idx,"Hospital.Name"]
+        result
+}
+
